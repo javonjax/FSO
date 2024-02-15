87cdc6139e2e94a3a63c9303e8df637ff1cf7967
javonjax <javonj461@hotmail.com>
2024-02-14 22:31:11 -0500

    Testing arrays

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const notes = [
+  {
+    id: 1,
+    content: 'HTML',
+    important: true
+  },
+  {
+    id: 2,
+    content: 'JS',
+    imporant: true
+  },
+  {
+    id: 3,
+    content: 'CSS',
+    important: true
+  }
+];
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App notes={notes} />)
